ec4b59e2a44777efd4084ceb5154e166b0160af6
grace-mao23 <gmao00@stuy.edu>
2019-04-19 19:13:25 -0400

    Drafted pushDown

--- MyHeap.java
+++ MyHeap.java
@@ -1,7 +1,39 @@
 public class MyHeap {
 
   private static void pushDown(int[]data,int size,int index) {
+    int child1 = index * 2 + 1;
+    int child2 = index * 2 + 2;
+    int i = index;
+    // not at a leaf
+    while (child1 < size && child2 < size) {
+      // none of the children are larger
+      if (data[child1] <= data[i] && data[child2] <= data[i]) {
+        child1 = size + 1; // will end while loop
+      } else if (data[child1] > data[i] && data[child2] > data[i]) {
+        // if both are larger
+        if (data[child1] > data[child2]) {
+          swap(data, i, child1);
+          i = child1;
+        } else {
+          swap(data, i, child2);
+          i = child2;
+        }
+      } else if (data[child1] > data[i]) {
+        swap(data, i, child1);
+        i = child1;
+      } else {
+        swap(data, i, child2);
+        i = child2;
+      }
+      child1 = i * 2 + 1;
+      child2 = i * 2 + 2;
+    }
+  }
 
+  private static void swap(int[] data, int one, int two) {
+    int temp = data[one];
+    data[one] = data[two];
+    data[two] = temp;
   }
 
   private static void pushUp(int[]data,int index) {
